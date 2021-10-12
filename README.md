bc1010c443a582dc72d9bb898001ce8c237ccd93
entr0pia <30486766+entr0pia@users.noreply.github.com>
2021-10-12 16:50:00 +0800

    修复权限

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 ## 使用方法
 
-- 快速使用: ```sudo bash -c "$(curl -sLf https://git.io/Jzprg)"```
+- 快速使用: ```bash -c "$(curl -sLf https://git.io/Jzprg)"```
 
 - 更多选择:
 
   ```shell
-  sudo bash quickDeb.sh [options]
+  bash quickDeb.sh [options]
   options:
     -S, --source-only	只换源, 不安装软件
     -A, --install-all	安装预定的所有软件
--- quickDeb.sh
+++ quickDeb.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/bash
 
 flag=$1
+# 判断权限
+p="sudo"
+user=$(whoami)
+if [ "$user" == "root" ]; then
+	p=
+fi
+function install_all(){
+	# 安装 SpaceVim
+	curl -sLf https://spacevim.org/cn/install.sh |sed -E 's/(https:\/\/(github|raw.githubusercontent))/https:\/\/ghproxy.com\/\1/g' | bash
+	# 安装 gdb 插件: gef
+	pip install capstone unicorn keystone-engine ropper
+	git clone https://ghproxy.com/https://github.com/hugsy/gef.git ~/.gdb-plugin/gef
+	echo "source ~/.gdb-plugin/gef/gef.py" >> ~/.gdbinit
+}
 
 # 换源
 id=$(cat /etc/os-release | grep ^ID= | cut -d'=' -f 2)
 codename=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d'=' -f2)
-echo https://mirrors.ustc.edu.cn/repogen/conf/$id-https-4-$codename | xargs curl -so /etc/apt/sources.list
+echo https://mirrors.ustc.edu.cn/repogen/conf/$id-https-4-$codename | xargs $p curl -so /etc/apt/sources.list
 
 if [ "$flag" == "-S" -o "$flag" == "--source-only" ]; then
 	exit
 fi
 
 # 安装软件
-apt update
-apt install git vim zsh python3 python3-pip python-is-python3 gcc make gdb openssh-server openssh-client -y
+$p apt update
+$p apt install git vim zsh python3 python3-pip python-is-python3 gcc make gdb openssh-server openssh-client -y
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 if [ "$flag" == "-A" -o "$flag" == "--install-all" ]; then
@@ -21,14 +35,6 @@ if [ "$flag" == "-A" -o "$flag" == "--install-all" ]; then
 fi
 
 # 安装 oh-my-zsh
-sh -c "$(curl -fsSL https://ghproxy.com/https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed -E 's/(https:\/\/(github|raw.githubusercontent))/https:\/\/ghproxy.com\/\1/g')"
-
-function install_all(){
-	# 安装 SpaceVim
-	curl -sLf https://spacevim.org/cn/install.sh |sed -E 's/(https:\/\/(github|raw.githubusercontent))/https:\/\/ghproxy.com\/\1/g' | bash
-	curl https://sh.rustup.rs -sSf | sh
-	# 安装 gdb 插件: gef
-	pip install capstone unicorn keystone-engine ropper
-	git clone https://ghproxy.com/https://github.com/hugsy/gef.git ~/.gdb-plugin/gef
-	echo "source ~/.gdb-plugin/gef/gef.py" >> ~/.gdbinit
-}
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://ghproxy.com/https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed -E 's/(https:\/\/(github|raw.githubusercontent))/https:\/\/ghproxy.com\/\1/g')"
+fi
